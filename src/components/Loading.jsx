60edf8d430265999d8bb38e07f72ca4d5922f117
Martin Aspeli <optilude@gmail.com>
2020-04-13 21:10:44 +0100

    Use a named function

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Grid, CircularProgress } from '@material-ui/core';
 
-export default () => (
-  <Grid
-    container
-    spacing={0}
-    direction="column"
-    alignItems="center"
-    justify="center"
-    style={{ minHeight: '100vh' }}
-  >
-    <Grid item xs={3}>
-      <CircularProgress />
-    </Grid>   
-</Grid> 
-);
+export default function Loading() {
+  return (
+    <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Grid item xs={3}>
+          <CircularProgress />
+        </Grid>   
+    </Grid> 
+  );
+}
