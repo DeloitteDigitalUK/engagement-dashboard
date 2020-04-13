efa3ae1a94548b3e89077bcc2a4eb61889e3ae8e
Martin Aspeli <optilude@gmail.com>
2020-04-13 21:59:52 +0100

    Centre text

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link as RouterLink } from "react-router-dom";
 
-import { Link, Grid } from '@material-ui/core';
+import { Link, Grid, makeStyles } from '@material-ui/core';
 import ErrorOutlinedIcon from '@material-ui/icons/ErrorOutlined';
 
 import AnonymousLayout from '../layouts/AnonymousLayout';
 
+const useStyles = makeStyles((theme) => ({
+  infoText: {
+    'text-align': 'center'
+  }
+}));
+
 export default function NotFoundPage() {
+
+  const styles = useStyles();
 
   return (
     <AnonymousLayout icon={<ErrorOutlinedIcon />} title="Not found">
       <Grid container spacing={2}>
-        <Grid item xs={12}>
+        <Grid item xs={12} className={styles.infoText}>
           <p>
             The page you tried to access cannot be found.
           </p>
