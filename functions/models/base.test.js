4c4bbf75ed8ad06f9ea265cb67f0c11858529e1d
Martin Aspeli <optilude@gmail.com>
2020-04-16 17:39:13 +0100

    it -> test

--- functions/models/base.test.js
+++ functions/models/base.test.js
@@ -12,23 +12,23 @@ class TestModel extends Model {
 }
 
 describe("constructor", () => {
-  it('can construct a valid object with defaults', () => {
+  test('can construct a valid object with defaults', () => {
     const t = new TestModel();
     expect(t.getId()).toEqual(null);
     expect(t.toObject()).toEqual({name: "Anonymous", email: ""});
   });
   
-  it('can construct a valid object with values', () => {
+  test('can construct a valid object with values', () => {
     const t = new TestModel("123", {name: "John", email: "test@example.org"});
     expect(t.getId()).toEqual("123");
     expect(t.toObject()).toEqual({name: "John", email: "test@example.org"});
   });
   
-  it('validates constructor arguments against schema', () => {
+  test('validates constructor arguments against schema', () => {
     expect(() => new TestModel("123", {name: "John", email: "notanemail"})).toThrow(Yup.ValidationError);
   });
   
-  it('strips unknown values', () => {
+  test('strips unknown values', () => {
     const t = new TestModel("123", {name: "John", email: "test@example.org", foo: "bar"});
     expect(t.getId()).toEqual("123");
     expect(t.toObject()).toEqual({name: "John", email: "test@example.org"});
@@ -36,7 +36,7 @@ describe("constructor", () => {
 });
 
 describe("updating the object", () => {
-  it('can update values', () => {
+  test('can update values', () => {
     const t = new TestModel("123", {name: "John", email: "test@example.org"});
 
     t.setId("321");
@@ -49,7 +49,7 @@ describe("updating the object", () => {
     expect(t.toObject()).toEqual({name: "James", email: "james@example.org"});
   });
 
-  it('can partially update the object', () => {
+  test('can partially update the object', () => {
     const t = new TestModel("123", {name: "John", email: "test@example.org"});
     
     t.update({
@@ -60,13 +60,13 @@ describe("updating the object", () => {
     expect(t.toObject()).toEqual({name: "James", email: "test@example.org"});
   });
   
-  it('validates on update', () => {
+  test('validates on update', () => {
     const t = new TestModel("123", {name: "John", email: "test@example.org"});
     
     expect(() => t.update({ email: "invalid" })).toThrow(Yup.ValidationError);
   });
 
-  it('ignores unknown values', () => {
+  test('ignores unknown values', () => {
     const t = new TestModel("123", {name: "John", email: "test@example.org"});
     
     t.setId("321");
@@ -81,17 +81,17 @@ describe("updating the object", () => {
 });
 
 describe("calculating paths", () => {
-  it('can calculate path', () => {
+  test('can calculate path', () => {
     const t = new TestModel("123");
     expect(t.getPath()).toEqual("testModels/123");
   });
   
-  it('cannot calculate path without id', () => {
+  test('cannot calculate path without id', () => {
     const t = new TestModel();
     expect(t.getPath).toThrow();
   });
   
-  it('can calculate path with parent', () => {
+  test('can calculate path with parent', () => {
     const t1 = new TestModel("123"),
           t2 = new TestModel("321");
     
@@ -100,19 +100,19 @@ describe("calculating paths", () => {
 });
 
 describe("the class is a Firestore converter", () => {
-  it('can convert to Firestore', () => {
+  test('can convert to Firestore', () => {
     const t = new TestModel("123", {name: "John", email: "test@example.org"});
     const c = TestModel.toFirestore(t);
     expect(c).toEqual({name: "John", email: "test@example.org"});
   });
   
-  it('can convert to Firestore with defaults', () => {
+  test('can convert to Firestore with defaults', () => {
     const t = new TestModel("123", {email: "test@example.org"});
     const c = TestModel.toFirestore(t);
     expect(c).toEqual({name: "Anonymous", email: "test@example.org"});
   });
   
-  it('can convert from Firestore', () => {
+  test('can convert from Firestore', () => {
   
     const fauxSnapshot = {
       data: (options) => ({name: "John", email: "test@example.org"}),
