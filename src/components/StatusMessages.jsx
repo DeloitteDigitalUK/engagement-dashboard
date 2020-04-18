3cd9671e5357231781d20db5e219792003bf0ce4
Martin Aspeli <optilude@gmail.com>
2020-04-18 21:17:42 +0100

    Remove useless styles

--- src/components/StatusMessages.jsx
+++ src/components/StatusMessages.jsx
@@ -3,18 +3,14 @@ import React from 'react';
 import { Box } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 
-import { useAuthenticatedStyles } from '../layouts/AuthenticatedLayout';
-
 /**
  * Display status messages as managed by `useStatusMessages()`
  */
 export default function StatusMessages({ messages }) {
-  const classes = useAuthenticatedStyles();
-
   return (
     <Box my={2}>
-      {messages.error && <Alert className={classes.alert} severity="error">{messages.error}</Alert>}
-      {messages.status && <Alert className={classes.alert} severity="success">{messages.status}</Alert>}
+      {messages.error && <Alert severity="error">{messages.error}</Alert>}
+      {messages.status && <Alert severity="success">{messages.status}</Alert>}
     </Box>
   );
 }
