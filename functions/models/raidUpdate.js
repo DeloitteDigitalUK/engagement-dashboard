26a680497eb5b125e192643d2e270fc734c38789
Martin Aspeli <optilude@gmail.com>
2020-04-27 09:59:03 +0100

    Rules and integration tests for all update types

--- functions/models/raidUpdate.js
+++ functions/models/raidUpdate.js
@@ -8,8 +8,8 @@ const raidUpdateSchema = Update.getSchema().concat(Yup.object({
   raidItems: Yup.array().of(Yup.object({
     type: Yup.string().oneOf(['risk', 'issue', 'assumption', 'dependency', 'decision']).required(),
     summary: Yup.string().required().default(""),
-    url: Yup.string().url().notRequired(),
-    priority: Yup.string().oneOf(['low', 'medium', 'high']).notRequired(),
+    url: Yup.string().url().notRequired().nullable().default(null),
+    priority: Yup.string().oneOf(['low', 'medium', 'high']).required().default('medium'),
     date: Yup.date().transform(transformDate).notRequired().nullable().default(null)
   })).required().default([])
 }));
